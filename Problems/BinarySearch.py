2ac0480a471117ec1c859dde9acb67c2c6aea5de
Abhirama Sonny <abhirama.sonny@gmail.com>
2023-10-14 07:52:23 -0500

    binsearch

--- Problems/BinarySearch.py
+++ Problems/BinarySearch.py
@@ -1,4 +1,16 @@
-def binarysearch(nums,target):
-    mid = len(nums)//2
-    print(nums[mid])
-binarysearch([1,2,3,4,5,6,7,8,9])
+def binarysearch(nums, target):
+    low = 0
+    high = len(nums) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+index = binarysearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 11)
+print(index)
